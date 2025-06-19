049e72272e258e0471c3bcc6b7c800abe826230d
evallugo <lugoe@rpi.edu>
2025-06-19 14:40:31 -0400

    fix

--- labs/8/js/projects.js
+++ labs/8/js/projects.js
@@ -37,7 +37,7 @@ $(document).ready(function() {
     error: function(xhr, status, error) {
       console.error('Error loading projects:', error);
       const fallbackButtons = `
-        <a href="../../teamproject/home/homepage.html" class="button">Beyond the Zodiac <i class="fa-solid fa-star"></i></a>
+        <a href="../../team project/home/homepage.html" class="button">Beyond the Zodiac <i class="fa-solid fa-star"></i></a>
         <a href="../../labs/3/lab2.html" class="button">Lab 2 <i class="fa-solid fa-folder"></i></a>
       `;
       
